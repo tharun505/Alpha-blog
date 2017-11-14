2f1dbdfcce7feef502838922d7697ee78173b7cf
tharun <tvavilapalli_gps@nec.edu>
2017-11-14 22:18:51 +0000

    Created User interface to input new articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   
   validates :title, presence: true, length: {minimum:3, maximum: 20}
-  validates :description, presence: true, length: {minimum:50, maximum: 300}
+  validates :description, presence: true, length: {minimum:5, maximum: 300}
   
   
 end
