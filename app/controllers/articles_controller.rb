d5729a4b1cae2e834a46989cb6227bf9036ce884
tharun <tvavilapalli_gps@nec.edu>
2017-11-15 21:22:33 +0000

    created edit option to the article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,32 +1,52 @@
 class ArticlesController < ApplicationController
 
-def new
-  @article = Article.new
+      def new
+           @article = Article.new
   
-end
-def create
+      end
+      
+      def create
 #render plain: params[:article].inspect
 
-@article = Article.new(article_params)
-if @article.save
-  flash.notice = "The article is succesfully created."
-redirect_to article_path(@article)
+        @article = Article.new(article_params)
+                if @article.save
+                 flash.notice = "The article is succesfully created."
+                 redirect_to article_path(@article)
 
-else 
-  render :new
-end
-end
+                else 
+                   render :new
+                end
+      end
 
-def show
-@article = Article.find(params[:id])
-end
+      def show
+        @article = Article.find(params[:id])
+      end
 
 
+    def edit
+        @article = Article.find(params[:id])
+    
+    end
+    
+    def update
+      @article = Article.find(params[:id])
+        if  @article.update(article_params)
+         
+        
+        flash.notice = "Article is succesfully updated"
+      
+        redirect_to article_path(@article)
+      else
+        
+        render :edit
+        end
+    end
+    
+    
+    private
 
-private
-
-def article_params
-params.require(:article).permit(:title, :description)
-end
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
 
 end
