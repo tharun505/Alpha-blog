28a49b2ad3fb5750da4444e8dff90f489654afb3
tharun <tvavilapalli_gps@nec.edu>
2017-11-15 18:41:23 +0000

    creation of show And fixed bugs

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,16 +8,25 @@ def create
 #render plain: params[:article].inspect
 
 @article = Article.new(article_params)
-@article.save
-redirect_to articles_show(@article)
+if @article.save
+  flash.notice = "The article is succesfully created."
+redirect_to article_path(@article)
+
+else 
+  render :new
 end
+end
+
+def show
+@article = Article.find(params[:id])
+end
+
+
 
 private
 
 def article_params
 params.require(:article).permit(:title, :description)
-  
 end
-
 
 end
